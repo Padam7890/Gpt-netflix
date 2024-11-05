d67bcc9065f6f2af61628537af7ccf004098c42d
Padam7890 <padamthapamoho@gmail.com>
2024-11-05 19:24:37 +0545

    newly added

---
+++ src/components/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+
+    <div className="login">
+    <form action="">
+      <label htmlFor="email">Email</label>
+      <input name='email' id='email' type="text" />
+    </form>
+    </div>
+    
+  )
+}
+
+export default Login
